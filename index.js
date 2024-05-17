e0be310a4ff20d4753cfa9f0fc9ce87b9075ec39
vishwa238mohan <vishwamohan238@gmail.com>
2024-05-17 18:28:38 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+
+const http = require('http');
+const port = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  const msg = 'Hello KOMAL, How are you. what are you doing now? \n'
+  res.end(msg);
+});
+
+server.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}/`);
+});
